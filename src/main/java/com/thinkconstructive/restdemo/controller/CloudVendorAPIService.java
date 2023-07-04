68fe23c3a71a76e6856ea4a4c7de7a49e13fa1eb
revianimarwan <revianimarwandhani30548@gmail.com>
2023-07-04 09:46:35 +0700

    Rest Demo

---
+++ src/main/java/com/thinkconstructive/restdemo/controller/CloudVendorAPIService.java
@@ -0,0 +1,28 @@
+package com.thinkconstructive.restdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thinkconstructive.restdemo.model.CloudVendor;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService {
+    CloudVendor cloudVendor;
+
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId) {
+        return cloudVendor;
+        // new CloudVendor("C1", "vendor 1", "Address One", "xxxx");
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Created Successfully";
+
+    }
+}
